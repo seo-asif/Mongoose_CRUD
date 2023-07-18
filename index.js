2f74a4dbd424c4e2bc9854f444d3a9bd591fc227
Asif Mosharraf <asif064@gmail.com>
2023-07-18 17:53:32 +0600

    GET API

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 
 const app = express();
+const PORT = 3000;
+
+const users = require("./users_db.json");
+
+
+//Get API METHOD
 app.get("/", (req, res) => {
   return res.send("Hello from Home page");
 });
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/users", (req, res) => {
+  const html = `<ul>
+    ${users
+      .map(
+        (user) => `
+      <li>${user.first_name} ${user.job_title}</li>`
+      )
+      .join("")}
+  </ul>`;
+  return res.send(html);
+});
+
+
+
+
 app.get("/about", (req, res) => {
   return res.send("Hello from about page");
 });
 
-app.listen(3000, () => console.log("server start on 3000"));
+app.listen(PORT, () => console.log("server start on 3000"));
